4800842081f38e7cf6328603457967e2e01eaf33
sturzael <esturz23@gmail.com>
2018-07-06 15:01:09 +1200

    image swap set

--- js/local_dev_anime.js
+++ js/local_dev_anime.js
@@ -3,7 +3,7 @@ var changeback;
 //loca dev js
 $('#link1').click(function() {
   $('#seo').attr('class', 'basic');
-  $('body').append("<style>@keyframes topan {0% {clip-path:polygon(" + setframe + ");background-image: url("+changeback+");}100%{clip-path: polygon(57% 0, 0 99%, 86% 77%, 100% 35%); background-image: url(img/dog.jpg);}</style>");
+  $('body').append("<style>@keyframes topan {0% {clip-path:polygon(" + setframe + ");background-image: url("+changeback+");}100%{clip-path: polygon(57% 0, 0 99%, 86% 77%, 100% 35%); background-image: url(img/jase.jpg);}</style>");
   $('#seo').addClass('animatetop');
   changeback = 'img/jase.jpg';
   setframe = '57% 0, 0 99%, 86% 77%, 100% 35%';
@@ -11,7 +11,7 @@ $('#link1').click(function() {
 
 $('#link2').click(function() {
   $('#seo').attr('class', 'basic');
-  $('body').append("<style>@keyframes seo {0% {clip-path:polygon(" + setframe + ");background-image: url("+changeback+");}100%{clip-path: polygon(0 28%, 19% 94%, 87% 100%, 100% 0);background-image: url(img/jase.jpg);}</style>");
+  $('body').append("<style>@keyframes seo {0% {clip-path:polygon(" + setframe + ");background-image: url("+changeback+");}100%{clip-path: polygon(0 28%, 19% 94%, 87% 100%, 100% 0);background-image: url(img/dog.jpg);}</style>");
   $('#seo').addClass('animateseo');
   changeback = 'img/dog.jpg';
   setframe = '0 28%, 19% 94%, 87% 100%, 100% 0';
@@ -19,16 +19,16 @@ $('#link2').click(function() {
 
 $('#link3').click(function() {
   $('#seo').attr('class', 'basic');
-  $('body').append("<style>@keyframes web {0% {clip-path:polygon(" + setframe + ");background-image: url("+changeback+");}100%{clip-path:polygon(21% 5%, 18% 72%, 73% 93%, 93% 20%);}</style>");
+  $('body').append("<style>@keyframes web {0% {clip-path:polygon(" + setframe + ");background-image: url("+changeback+");}100%{clip-path:polygon(21% 5%, 18% 72%, 73% 93%, 93% 20%);background-image: url(img/br.jpg);}</style>");
   $('#seo').addClass('animateweb');
-    changeback = 'img/pj.jpg';
+    changeback = 'img/br.jpg';
   setframe = '21% 5%, 18% 72%, 73% 93%, 93% 20%';
 });
 
 $('#link4').click(function() {
   $('#seo').attr('class', 'basic');
-  $('body').append("<style>@keyframes design {0% {clip-path:polygon(" + setframe + ");background-image: url("+changeback+");}100%{clip-path:polygon(23% 24%, 5% 85%, 100% 100%, 80% 0);}</style>");
+  $('body').append("<style>@keyframes design {0% {clip-path:polygon(" + setframe + ");background-image: url("+changeback+");}100%{clip-path:polygon(23% 24%, 5% 85%, 100% 100%, 80% 0);background-image: url(img/pj.jpg);}</style>");
   $('#seo').addClass('animatedesign');
-  changeback = 'img/br.jpg';
+  changeback = 'img/pj.jpg';
   setframe = '23% 24%, 5% 85%, 100% 100%, 80% 0';
 });
