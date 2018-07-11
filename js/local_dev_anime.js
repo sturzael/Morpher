3be4540f6168e23903e94be8b59ecc2dabee5232
sturzael <esturz23@gmail.com>
2018-07-11 12:44:20 +1200

    moved inside ajax request

--- js/local_dev_anime.js
+++ js/local_dev_anime.js
@@ -2,7 +2,6 @@ var changeback = 'https://www.nettl.com/nz/wp-content/uploads/2018/06/Nettl-Team
 var seo = $('#seo');
 var body = $('body');
 var setframe = '23% 24%, 5% 85%, 100% 100%, 80% 0';
-// displaynonetxt(thistext, secondtext);
 
 $.ajax({
   url: "./data/studio_members.json",
@@ -16,21 +15,20 @@ $.ajax({
     for (var i = 0; i < team.length; i++) {
       $('.middle').append("<li class='member' id='link" + [i] + "'>" + team[i].title + "</li>");
     }
-    morph();
+    $('.member').click(function() {
+      el = this.id.replace(/[a-z]/g, '');
+      $('.animationclass').remove();
+      body.append("<style class='animationclass'>.animater" + el + "{animation: animate" + el + " 1s; animation-fill-mode: forwards;}@keyframes animate" + el + " {0% {clip-path:polygon(" + setframe + ");background-image: url(" + changeback + ");}100%{clip-path: polygon(" + team[el].clipPath + "); background-image: url(" + team[el].photourl + ");}</style>");
+      seo.addClass('animater' + el);
+      changeback = "'" + team[el].photourl + "'";
+      setframe = team[el].clipPath;
+    });
   },
   error: function() {
     console.log("Something Went Wrong");
   }
 });
 
-
 function morph() {
-  $('.member').click(function() {
-    el = this.id.replace(/[a-z]/g, '');
-    $('.animationclass').remove();
-    body.append("<style class='animationclass'>.animater" + el + "{animation: animate" + el + " 1s; animation-fill-mode: forwards;}@keyframes animate" + el + " {0% {clip-path:polygon(" + setframe + ");background-image: url(" + changeback + ");}100%{clip-path: polygon(" + team[el].clipPath + "); background-image: url(" + team[el].photourl + ");}</style>");
-    seo.addClass('animater' + el);
-    changeback = "'" + team[el].photourl + "'";
-    setframe = team[el].clipPath;
-  });
+
 }
