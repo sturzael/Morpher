c4eb9e625e2fc10e77d80179d9666239f1db685d
sturzael <esturz23@gmail.com>
2018-07-11 13:39:24 +1200

    cleaning

--- js/local_dev_anime.js
+++ js/local_dev_anime.js
@@ -1,12 +1,12 @@
 var changeback = 'https://www.nettl.com/nz/wp-content/uploads/2018/06/Nettl-Team-June-2018-Jason-008-e1529617894224.jpg'; //default image used for the first animation
-var seo = $('#seo'); //the div that morphs
+var morpher = $('#morpher'); //the div that morphs
 var body = $('body');
 var setframe = '23% 24%, 5% 85%, 100% 100%, 80% 0'; //default clip path used for the very first animation
 $.ajax({ //pull the data from the json file - The json file should contain all data relevant to the studio - each studio will have a different json file
   url: "./data/studio_members.json",
   dataType: "json",
   beforeSend: function(xhr) {
-    if (xhr.overrideMimeType) {//xpecify file type as json
+    if (xhr.overrideMimeType) {//specify file type as json
       xhr.overrideMimeType("application/json");
     }
   },
@@ -18,7 +18,7 @@ $.ajax({ //pull the data from the json file - The json file should contain all d
       el = this.id.replace(/[a-z]/g, '');//remove all text from id - will turn 'link1' into '1'
       $('.animationclass').remove(); //remove the old style animation from the body
       body.append("<style class='animationclass'>.animater" + el + "{animation: animate" + el + " 1s; animation-fill-mode: forwards;}@keyframes animate" + el + " {0% {clip-path:polygon(" + setframe + ");background-image: url(" + changeback + ");}100%{clip-path: polygon(" + team[el].clipPath + "); background-image: url(" + team[el].photoUrl + ");}</style>"); // append the animation and styling using json data
-      seo.addClass('animater' + el); //make animation happen
+      morpher.addClass('animater' + el); //make animation happen
       changeback = "'" + team[el].photoUrl + "'"; //set this variable as the old image so that the next animation can reference it
       setframe = team[el].clipPath; //set this variable as the old clip path so that the next animation can reference it
     });
