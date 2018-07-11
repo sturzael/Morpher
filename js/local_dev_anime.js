001499ff183b74150a1d562b70e0e052c702943e
sturzael <esturz23@gmail.com>
2018-07-11 13:04:13 +1200

    zaks spelling mistakes

--- js/local_dev_anime.js
+++ js/local_dev_anime.js
@@ -1,8 +1,8 @@
-var changeback = 'https://www.nettl.com/nz/wp-content/uploads/2018/06/Nettl-Team-June-2018-Jason-008-e1529617894224.jpg';//defualt image used for the first animation
+var changeback = 'https://www.nettl.com/nz/wp-content/uploads/2018/06/Nettl-Team-June-2018-Jason-008-e1529617894224.jpg'; //default image used for the first animation
 var seo = $('#seo'); //the div that morphs
 var body = $('body');
 var setframe = '23% 24%, 5% 85%, 100% 100%, 80% 0'; //default clip path used for the very first animation
-$.ajax({//pull the data from the json file - The json file should contain all data relevant to the studio - each studio willl have a differant json file
+$.ajax({ //pull the data from the json file - The json file should contain all data relevant to the studio - each studio will have a different json file
   url: "./data/studio_members.json",
   dataType: "json",
   beforeSend: function(xhr) {
@@ -16,11 +16,11 @@ $.ajax({//pull the data from the json file - The json file should contain all da
     }
     $('.member').click(function() { //each time you click on li do this
       el = this.id.replace(/[a-z]/g, '');//remove all text from id - will turn 'link1' into '1'
-      $('.animationclass').remove();//remove the old style animation from the body
-      body.append("<style class='animationclass'>.animater" + el + "{animation: animate" + el + " 1s; animation-fill-mode: forwards;}@keyframes animate" + el + " {0% {clip-path:polygon(" + setframe + ");background-image: url(" + changeback + ");}100%{clip-path: polygon(" + team[el].clipPath + "); background-image: url(" + team[el].photourl + ");}</style>"); // append the aniamtion and styling using json data
-      seo.addClass('animater' + el);//make animation happen
-      changeback = "'" + team[el].photourl + "'";//set this varible as the old image so that the next animation can reference it
-      setframe = team[el].clipPath;//set this variable as the old clip path so that the next animation can reference it
+      $('.animationclass').remove(); //remove the old style animation from the body
+      body.append("<style class='animationclass'>.animater" + el + "{animation: animate" + el + " 1s; animation-fill-mode: forwards;}@keyframes animate" + el + " {0% {clip-path:polygon(" + setframe + ");background-image: url(" + changeback + ");}100%{clip-path: polygon(" + team[el].clipPath + "); background-image: url(" + team[el].photoUrl + ");}</style>"); // append the animation and styling using json data
+      seo.addClass('animater' + el); //make animation happen
+      changeback = "'" + team[el].photoUrl + "'"; //set this variable as the old image so that the next animation can reference it
+      setframe = team[el].clipPath; //set this variable as the old clip path so that the next animation can reference it
     });
   },
   error: function() {
